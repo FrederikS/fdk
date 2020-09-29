a752fe1e33cf4a8f3ab89145c004989547776526
frederik.steffen <frederik.steffen@gmail.com>
2020-09-29 13:57:20 +0200

    added more tracking

---
+++ src/components/header-external-links.tsx
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, Link as TLink } from "theme-ui";
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
+
+const HeaderExternalLinks = () => {
+  const { externalLinks } = useMinimalBlogConfig();
+
+  return (
+    <React.Fragment>
+      {externalLinks && externalLinks.length > 0 && (
+        <div sx={{ "a:not(:first-of-type)": { ml: 3 }, fontSize: [1, `18px`] }}>
+          {externalLinks.map((link) => (
+            <TLink as={OutboundLink} key={link.url} href={link.url}>
+              {link.name}
+            </TLink>
+          ))}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default HeaderExternalLinks;
